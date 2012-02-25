545d6ea40071291a7a3d5dea81615afea68cd10e
Stuart Wheater <stuart.wheater@gmail.com>
2012-02-25 18:03:54 +0000

    Rename structure variable names

--- dev/Kernel/ELF32.h
+++ dev/Kernel/ELF32.h
@@ -5,29 +5,29 @@
  * Copyright (c) 2012, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
  */
 
-#define ELF_HEADER_MAGIC_LENGTH 4
-#define ELF_HEADER_PADDING_LENGTH 9
+#define ELF32_HEADER_MAGIC_LENGTH 4
+#define ELF32_HEADER_PADDING_LENGTH 9
 
 typedef struct
 {
-    char           magic[ELF_HEADER_MAGIC_LENGTH];
-    unsigned char  class;
-    unsigned char  byteorder;
-    unsigned char  headerversion;
-    char           padding[ELF_HEADER_PADDING_LENGTH];
+    char           magic[ELF32_HEADER_MAGIC_LENGTH];
+    unsigned char  fileClass;
+    unsigned char  byteOrder;
+    unsigned char  headerVersion;
+    char           padding[ELF32_HEADER_PADDING_LENGTH];
     unsigned short type;
     unsigned short machine;
     unsigned long  version;
     unsigned int   entry;
-    unsigned int   programheaderoffset;
-    unsigned int   sectionheaderoffset;
+    unsigned int   programHeaderOffset;
+    unsigned int   sectionHeaderOffset;
     unsigned long  flags;
     unsigned short headersize;
-    unsigned short programheaderentrysize;
-    unsigned short programheadernumber;
-    unsigned short sectionheaderentrysize;
-    unsigned short sectionheaderentrynumber;
-    unsigned short sectionheadertableindex;
+    unsigned short programHeaderEntrySize;
+    unsigned short programHeaderNumber;
+    unsigned short sectionHeaderEntrySize;
+    unsigned short sectionHeaderEntryNumber;
+    unsigned short sectionHeaderTableIndex;
 } ELF32Header;
 
 typedef struct
@@ -40,8 +40,8 @@ typedef struct
     unsigned long size;
     unsigned long link;
     unsigned long info;
-    unsigned long addressalign;
-    unsigned long entrysize;
+    unsigned long addressAlign;
+    unsigned long entrySize;
 } ELF32SectionHeader;
 
 typedef struct
@@ -51,7 +51,7 @@ typedef struct
     unsigned long  size;
     unsigned char  info;
     unsigned char  other;
-    unsigned short sectionheadertableindex;
+    unsigned short sectionHeaderTableIndex;
 } ELF32SymbolEntry;
 
 typedef struct
@@ -73,8 +73,8 @@ typedef struct
     unsigned int  offset;
     unsigned int  vaddr;
     unsigned int  paddr;
-    unsigned long filesize;
-    unsigned long memsize;
+    unsigned long fileSize;
+    unsigned long memSize;
     unsigned long flags;
     unsigned long align;
 } ELF32ProgramHeader;

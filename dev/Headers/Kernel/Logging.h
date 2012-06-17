f483d10e09d566f7877f9547fd3532f3a353f8f2
Stuart Wheater <stuart.wheater@gmail.com>
2012-06-17 23:14:50 +0100

    Additional Logging support

--- dev/Headers/Kernel/Logging.h
+++ dev/Headers/Kernel/Logging.h
@@ -5,6 +5,9 @@
  * Copyright (c) 2012, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
  */
 
+#include <Kernel/StdTypes.h>
+
+extern void logUnsignedWord32Hex(UnsignedWord32 unsignedWord32);
 extern void logMessage(const char *message);
 
 #endif
--- dev/Source/Kernel/Logging.c
+++ dev/Source/Kernel/Logging.c
@@ -1,13 +1,30 @@
 /*
  * Copyright (c) 2012, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+
  */
 
-#include <Kernel/Logging.h>
+#include <Kernel/StdTypes.h>
 #include <Device/RaspPi_UART.h>
+#include <Kernel/Logging.h>
+
+void logUnsignedWord32Hex(UnsignedWord32 unsignedWord32)
+{
+    int index;
+    for (index = 0; index < 8; index++)
+    {
+        unsigned int digit = unsignedWord32 >> 28;
+        if (digit < 10)
+            uartOutput(digit + '0');
+        else
+            uartOutput(digit + 'A' - 10);
+        unsignedWord32 <<= 4;
+    }
+}
+
 
 void logMessage(const char *message)
 {
-    while (*message != 0)
+    while (*message != 0) 
     {
         uartOutput(*message);
         message++;

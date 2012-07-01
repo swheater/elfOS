e41ac7102638af962c4f31e5b3d6de36976f2c60
Stuart Wheater <stuart.wheater@gmail.com>
2012-07-01 01:54:51 +0100

    Adding Log to elfOS

---
+++ dev/Headers/elfOS/Log.h
@@ -0,0 +1,13 @@
+#ifndef _ELFOS_LOG
+#define _ELFOS_LOG 1
+
+/*
+ * Copyright (c) 2012, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+#include <Kernel/StdTypes.h>
+
+extern void elfOS_logUnsignedWord32Hex(UnsignedWord32 unsignedWord32);
+extern void elfOS_logMessage(const char *message);
+
+#endif

4b88184cad64c84a012ce7d545f60a7cac675f6c
Stuart Wheater <stuart.wheater@gmail.com>
2012-09-20 00:57:56 +0100

    Added Log.c

---
+++ dev/Source/elfOS/Log.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+#include <elfOS/Log.h>
+
+void elfOS_logUnsignedWord32Hex(UnsignedWord32 unsignedWord32)
+{
+    asm("swi\t#0x000041");
+}
+
+void elfOS_logMessage(const char *message)
+{
+    asm("swi\t#0x000042");
+}

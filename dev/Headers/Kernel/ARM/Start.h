d2bd2ed213abd7171726b5d37a540ee6f26a797e
Stuart Wheater <stuart.wheater@gmail.com>
2013-06-30 17:14:13 +0100

    Initial version

---
+++ dev/Headers/Kernel/ARM/Start.h
@@ -0,0 +1,10 @@
+#ifndef _KERNEL_START
+#define _KERNEL_START 1
+
+/*
+ * Copyright (c) 2012-2013, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+extern void kernel_boot_handlerStacksSetup(void);
+
+#endif

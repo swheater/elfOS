89e056ee23422abccfc74fcd2a2f69fdd4bfaca2
Stuart Wheater <stuart.wheater@gmail.com>
2013-09-08 18:36:11 +0100

    Update of init sequence

--- dev/Source/Device/SPI_SecureDigital.c
+++ dev/Source/Device/SPI_SecureDigital.c
@@ -16,7 +16,7 @@ static void logData(const char *message, UnsignedWord32 data[], UnsignedWord32 d
     UnsignedWord32 dataIndex;
     for (dataIndex = 0; dataIndex < dataLength; dataIndex++)
     {
-        if ((dataIndex % 7) == 0)
+        if ((dataIndex % 16) == 0)
             logMessage("\r\n");
 
         logMessage(" ");
@@ -29,45 +29,52 @@ void securedigitalInit(void)
 {
     spiSetClockRate(400000);
 
-    UnsignedWord32 outputData[14];
-    UnsignedWord32 inputData[14];
+    UnsignedWord32 outputData[10];
+    UnsignedWord32 inputData[10];
 
     UnsignedWord32 dataIndex;
-    for (dataIndex = 0; dataIndex < 14; dataIndex++)
-       outputData[dataIndex] = 0xFF;
-
-    logData("State-up, Output", outputData, 14);
-    spiTransfer(2, outputData, inputData, 14);
-    logData("Start-up, Input", inputData, 14);
-}
-
-void securedigitalSendCommand(UnsignedByte command, UnsignedByte args[6], UnsignedByte crc)
-{
-    UnsignedWord32 outputData[42];
-    UnsignedWord32 inputData[42];
-
-    UnsignedWord32 dataIndex;
-    for (dataIndex = 0; dataIndex < 42; dataIndex++)
+    for (dataIndex = 0; dataIndex < 10; dataIndex++)
         outputData[dataIndex] = 0xFF;
 
-    outputData[0] = (0x000000C0 & command) | 0x00000040;
-    UnsignedWord32 argsIndex;
-    for (argsIndex = 0; argsIndex < 6; argsIndex++)
-        outputData[argsIndex + 1] = args[argsIndex];
-    outputData[7] = (crc << 1) | 0x00000001;
+    spiTransfer(2, outputData, inputData, 10);
+}
 
-    logData("Command", outputData, 8);
-    spiTransfer(0, outputData, inputData, 42);
-    logData("Command", inputData, 42);
+void securedigitalSendCommand(UnsignedByte command, UnsignedByte args[4], UnsignedByte crc)
+{
+    UnsignedWord32 outputData[6];
+    UnsignedWord32 inputData[1];
+
+    outputData[0] = (0x0000003F & command) | 0x00000040;
+    UnsignedWord32 argsIndex;
+    for (argsIndex = 0; argsIndex < 4; argsIndex++)
+        outputData[argsIndex + 1] = args[argsIndex];
+    outputData[5] = (crc << 1) | 0x00000001;
+
+    logData("Request", outputData, 6);
+    spiAsyncTransfer(0, outputData, inputData, 6, 1);
+    logData("Response", inputData, 1);
 }
 
 void securedigitalTest(void)
 {
-    UnsignedByte args[6];
+    UnsignedByte args[4];
 
     UnsignedWord32 argsIndex;
-    for (argsIndex = 0; argsIndex < 6; argsIndex++)
+    for (argsIndex = 0; argsIndex < 4; argsIndex++)
         args[argsIndex] = 0x00;
 
-    securedigitalSendCommand(0, args, 0x00);
+    securedigitalSendCommand(0, args, 0x4A);
+
+    securedigitalSendCommand(0, args, 0x4A);
+
+    UnsignedWord32 count = 0;
+    while (count < 8)
+    {
+        securedigitalSendCommand(55, args, 0x00);
+
+        securedigitalSendCommand(41, args, 0x00);
+
+        logUnsignedWord32Hex(count++);
+        logMessage("\r\n");
+    }
 }

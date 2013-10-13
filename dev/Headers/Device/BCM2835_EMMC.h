f67c2ad02aa16ee610fb755d7e470814b9d464a3
Stuart Wheater <stuart.wheater@gmail.com>
2013-10-13 22:55:29 +0100

    Initial Template for EMMC support

---
+++ dev/Headers/Device/BCM2835_EMMC.h
@@ -0,0 +1,13 @@
+#ifndef _DEVICE_BCM2835_EMMC_H
+#define _DEVICE_BCM2835_EMMC_H 1
+
+/*
+ * Copyright (c) 2012-2013, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+#include <Kernel/StdTypes.h>
+
+extern void emmcInit(void);
+extern void emmcShutdown(void);
+
+#endif
---
+++ dev/Source/Device/BCM2835_EMMC.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012-2013, Stuart Wheater, Newcastle upon Tyne, England. All rights reserved.
+ */
+
+// Device driver for BCM2835 - EMMC (External Mass Media Controller)
+
+#include <Kernel/StdTypes.h>
+#include <Kernel/Logging.h>
+#include <Device/BCM2835_EMMC.h>
+
+#define EMMC_BASE    ((volatile UnsignedWord32*) 0x20300000)
+#define EMMC__OFFSET (0x00)
+
+void emmcInit(void)
+{
+}
+
+void emmcShutdown(void)
+{
+}

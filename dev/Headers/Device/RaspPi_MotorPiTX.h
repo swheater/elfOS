d47d5e4c9b8263da2fac72f2481af12e949b2ca1
Stuart Wheater <stuart.wheater@gmail.com>
2013-11-24 13:27:21 +0000

    Added Function Selection

--- dev/Headers/Device/RaspPi_MotorPiTX.h
+++ dev/Headers/Device/RaspPi_MotorPiTX.h
@@ -11,11 +11,14 @@
 #define MOTORPITX_OUTPUT2 (2)
 #define MOTORPITX_INPUT1  (1)
 #define MOTORPITX_INPUT2  (2)
+#define MOTORPITX_SERVO1  (1)
+#define MOTORPITX_SERVO2  (2)
 
 extern void    motorpitxInit(void);
 extern void    motorpitxSetReadyLED(Boolean on);
 extern void    motorpitxSetOutput(UnsignedByte outputNumber, Boolean on);
 extern Boolean motorpitxGetInput(UnsignedByte inputNumber);
+extern void    motorpitxSetServo(UnsignedByte servoNumber, UnsignedWord32 mark);
 extern void    motorpitxShutdown(void);
 
 #endif

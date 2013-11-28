4fdf389f29cf690a6efb711e673875d51904ea9d
Stuart Wheater <stuart.wheater@gmail.com>
2013-11-28 23:04:40 +0000

    Rework to use utility functions

--- dev/Source/Device/BCM2835_miniUART.c
+++ dev/Source/Device/BCM2835_miniUART.c
@@ -5,20 +5,8 @@
 // Device driver for BCM2835 - mini UART
 
 #include <Kernel/StdTypes.h>
+#include <Device/BCM2835_GPIO.h>
 #include <Device/BCM2835_miniUART.h>
-
-#define GPIO_BASE                              ((volatile UnsignedWord32*) 0x20200000)
-#define GPIO_FUNCSELECT_BASE                   (GPIO_BASE + 0x00)
-#define GPIO_FUNCSELECT_1_OFFSET               (0x01)
-#define GPIO_PINPULLUPDOWNENABLE_OFFSET        (0x25)
-#define GPIO_PINPULLUPDOWNENABLECLOCK_0_OFFSET (0x26)
-
-#define GPIO_FUNCSELECT_1_GPIO14_MASK              (0xFFFF8FFF)
-#define GPIO_FUNCSELECT_1_GPIO14_ALT5FUNC          (0x00002000)
-#define GPIO_PINPULLUPDOWNENABLE_OFF               (0x00000000)
-#define GPIO_PINPULLUPDOWNENABLECLOCK_0_GPIO14_BIT (0x00004000)
-#define GPIO_PINPULLUPDOWNENABLECLOCK_0_NONE_BITS  (0x00000000)
-#define GPIO_PINPULLUPDOWN_SETUPDELAY              (150)
 
 #define AUX_BASE                      ((volatile UnsignedWord32*) 0x20215000)
 #define AUX_INTERRUPTSTATUS_OFFSET    (0x00)
@@ -39,6 +27,11 @@
 #define AUX_ENABLES_UART_BIT           (0x00000001)
 #define UART_LINESTATUS_TXQNOTFULL_BIT (0x00000020)
 
+#define UART_TRANSMIT_GPIO (14)
+#define UART_RECEIVE_GPIO  (15)
+
+#define BASEBAUD_RATE (31250000)
+
 void uartInit(void)
 {
     *(AUX_BASE + AUX_ENABLES_OFFSET) |= AUX_ENABLES_UART_BIT; // Enable UART
@@ -48,23 +41,23 @@ void uartInit(void)
     *(UART_BASE + UART_LINECONTROL_OFFSET)       = 0x03;      // Select 8 bit 
     *(UART_BASE + UART_INTERRUPTIDENTIFY_OFFSET) = 0xC6;      // Clear receive and transmit FIFO queue
     *(UART_BASE + UART_MODEMCONTROL_OFFSET)      = 0x00;      // RTS to high
-    *(UART_BASE + UART_BAUDRATE_OFFSET)          = 0x010E;    // Set baud rate of 115200 pulses per second
-//    *(UART_BASE + UART_BAUDRATE_OFFSET)          = 0x0CB6;    // Set baud rate of 9600 pulses per second
 
-    UnsignedWord32 funcSelect1 = *(GPIO_FUNCSELECT_BASE + GPIO_FUNCSELECT_1_OFFSET);
-    funcSelect1 &= GPIO_FUNCSELECT_1_GPIO14_MASK;
-    funcSelect1 |= GPIO_FUNCSELECT_1_GPIO14_ALT5FUNC;
-    *(GPIO_FUNCSELECT_BASE + GPIO_FUNCSELECT_1_OFFSET) = funcSelect1;
+    uartSetBaudRate(115200);
 
-    volatile unsigned int delayCount;
-    *(GPIO_BASE + GPIO_PINPULLUPDOWNENABLE_OFFSET)        = GPIO_PINPULLUPDOWNENABLE_OFF;
-    for (delayCount = 0; delayCount < GPIO_PINPULLUPDOWN_SETUPDELAY; delayCount++);
-    *(GPIO_BASE + GPIO_PINPULLUPDOWNENABLECLOCK_0_OFFSET) = GPIO_PINPULLUPDOWNENABLECLOCK_0_GPIO14_BIT;
-    for (delayCount = 0; delayCount < GPIO_PINPULLUPDOWN_SETUPDELAY; delayCount++);
-    *(GPIO_BASE + GPIO_PINPULLUPDOWNENABLE_OFFSET)        = GPIO_PINPULLUPDOWNENABLE_OFF;
-    *(GPIO_BASE + GPIO_PINPULLUPDOWNENABLECLOCK_0_OFFSET) = GPIO_PINPULLUPDOWNENABLECLOCK_0_NONE_BITS;
+    gpioFuncSelect(UART_TRANSMIT_GPIO,     GPIO_FUNCSELECT_ALTFUNC5);
+    gpioSetPullControl(UART_TRANSMIT_GPIO, GPIO_PULL_OFF);
+    gpioFuncSelect(UART_RECEIVE_GPIO,      GPIO_FUNCSELECT_ALTFUNC5);
+    gpioSetPullControl(UART_RECEIVE_GPIO,  GPIO_PULL_OFF);
 
-    *(UART_BASE + UART_EXTRACONTROL_OFFSET)      = 0x02;      // Enable transmit only
+    *(UART_BASE + UART_EXTRACONTROL_OFFSET) = 0x02; // Enable transmit only
+}
+
+UnsignedWord32 uartSetBaudRate(UnsignedWord32 baudRate)
+{
+    UnsignedWord32 baudRateReg = (BASEBAUD_RATE / baudRate) - 1;
+    *(UART_BASE + UART_BAUDRATE_OFFSET) = baudRateReg;
+
+    return BASEBAUD_RATE / (baudRateReg + 1);
 }
 
 void uartOutput(UnsignedByte ch)
@@ -74,4 +67,8 @@ void uartOutput(UnsignedByte ch)
 
     // Write byte to data register
     *(UART_BASE + UART_IODATA_OFFSET) = ch;
+}
+
+void uartShutdown(void)
+{
 }

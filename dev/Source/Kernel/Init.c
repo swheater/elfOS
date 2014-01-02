9002a8042fa7ce8a6380ed9d47c580b5f55c06ae
Stuart Wheater <stuart.wheater@gmail.com>
2014-01-02 23:56:41 +0000

    Change test

--- dev/Source/Kernel/Init.c
+++ dev/Source/Kernel/Init.c
@@ -14,8 +14,9 @@
 #include <Device/BCM2835_BSC.h>
 #include <Device/BCM2835_Timer.h>
 #include <Device/BCM2835_SystemTimer.h>
-#include <Device/RaspPi_DisplayTFT18.h>
 #include <Device/BCM2835_SPI.h>
+#include <Device/BCM2835_PWM.h>
+#include <Device/RaspPi_DisplayTFT18.h>
 #include <Device/RaspPi_ILI9320.h>
 #include <Device/RaspPi_SSD1351.h>
 #include <Device/RaspPi_XPT2046.h>
@@ -62,16 +63,35 @@ void kernel_start(void)
 
     statusInit();
     systemtimerInit();
-    gpioInit();
-    spiInit();
-    uartInit();
     threadsInit();
     timerInit(127, 1000000, TRUE);
 
-    ssd1351Init(FALSE);
-    ilsoftoledInit();
+    uartInit();
+    spiInit();
+    spiSetClockRate(500000);
 
-    ilsoftoledTest();
+    UnsignedByte chipSelect = 1;
+    UnsignedByte outputData[3];
+    UnsignedByte inputData[3];
+
+    while (TRUE)
+    {
+        outputData[0] = 0x40;
+        outputData[1] = 0x01;
+        outputData[2] = 0x00;
+        spiTransfer(chipSelect, outputData, inputData, 3);
+        logData("Directions B (out)", outputData, 3);
+        logData("Directions B (in )", inputData, 3);
+
+        outputData[0] = 0x40;
+        outputData[1] = 0x15;
+        outputData[2] = 0xFF;
+        spiTransfer(chipSelect, outputData, inputData, 3);
+        logData("Output A (out)", outputData, 3);
+        logData("Output A (in )", inputData, 3);
+
+        systemtimerWait(1000000);
+    }
 
     UnsignedWord32 reg = 0;
     while (TRUE)
